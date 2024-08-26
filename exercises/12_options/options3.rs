f3588ae34e0f935e1ad448d46ddf323778e3d151
William Setiawan <z5388080@ad.unsw.edu.au>
2024-08-27 02:03:03 +1000

    Completed section 11 - 15

--- exercises/12_options/options3.rs
+++ exercises/12_options/options3.rs
@@ -9,6 +9,12 @@ fn main() {
 
     // TODO: Fix the compiler error by adding something to this match statement.
     match optional_point {
+        Some(ref p) => println!("Co-ordinates are {},{}", p.x, p.y),
+        _ => panic!("No match!"),
+    }
+
+    // Without using ref
+    match &optional_point {
         Some(p) => println!("Co-ordinates are {},{}", p.x, p.y),
         _ => panic!("No match!"),
     }
--- exercises/quizzes/quiz2.rs
+++ exercises/quizzes/quiz2.rs
@@ -27,7 +27,32 @@ mod my_module {
     use super::Command;
 
     // TODO: Complete the function as described above.
-    // pub fn transformer(input: ???) -> ??? { ??? }
+    pub fn transformer(input: Vec<(String, Command)>) -> Vec<String> {
+        let mut output = Vec::new();
+
+        for (input_str, cmd) in input {
+            let new_str = match cmd {
+                Command::Uppercase => input_str.to_uppercase(),
+                Command::Trim => input_str.trim().to_string(),
+                Command::Append(n) => input_str + &"bar".repeat(n),
+            };
+
+            output.push(new_str)
+        }
+
+        output
+    }
+
+    pub fn transformer_iter(input: Vec<(String, Command)>) -> Vec<String> {
+        input
+            .into_iter()
+            .map(|(input_str, cmd)| match cmd {
+                Command::Uppercase => input_str.to_uppercase(),
+                Command::Trim => input_str.trim().to_string(),
+                Command::Append(n) => input_str + &"bar".repeat(n),
+            })
+            .collect()
+    }
 }
 
 fn main() {
@@ -37,27 +62,29 @@ fn main() {
 #[cfg(test)]
 mod tests {
     // TODO: What do we need to import to have `transformer` in scope?
-    // use ???;
+    use super::my_module::{transformer, transformer_iter};
     use super::Command;
 
     #[test]
     fn it_works() {
-        let input = vec![
-            ("hello".to_string(), Command::Uppercase),
-            (" all roads lead to rome! ".to_string(), Command::Trim),
-            ("foo".to_string(), Command::Append(1)),
-            ("bar".to_string(), Command::Append(5)),
-        ];
-        let output = transformer(input);
+        for transformer in [transformer, transformer_iter] {
+            let input = vec![
+                ("hello".to_string(), Command::Uppercase),
+                (" all roads lead to rome! ".to_string(), Command::Trim),
+                ("foo".to_string(), Command::Append(1)),
+                ("bar".to_string(), Command::Append(5)),
+            ];
+            let output = transformer(input);
 
-        assert_eq!(
-            output,
-            [
-                "HELLO",
-                "all roads lead to rome!",
-                "foobar",
-                "barbarbarbarbarbar",
-            ]
-        );
+            assert_eq!(
+                output,
+                [
+                    "HELLO",
+                    "all roads lead to rome!",
+                    "foobar",
+                    "barbarbarbarbarbar",
+                ]
+            );
+        }
     }
 }
